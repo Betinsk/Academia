dd9f54f8b13e0d2bd94c9fa737d7dc5c04f07f64
betinsk <gmdust@hotmail.com>
2023-11-19 11:05:26 -0300

    workout

--- exercicios.js
+++ exercicios.js
@@ -1,33 +1,12 @@
 
-const chest = [
-    { 
-        name: 'Voador',
-        video: 'https://www.youtube.com/embed/J1YnmuweOkM?si=Nplw3bRZMWZTDEP6',
-        series: '3',
-        times: ' 10 to 12'
-    },
-
-    {
-        name: 'Incline bench press',
-        video: 'https://www.youtube.com/embed/Ky_JXqloq0w?si=3M07xaEI0IfMgFxr',
-        series: '3',
-        times: ' 10 to 12'
-    },
-
-    {
-        name: 'Crosshover',
-        video: 'https://www.youtube.com/embed/3iHf0jeHYNg?si=M6C2Vm5wuWemplq2',
-        series: '3',
-        times: ' 10 to 12'
-    }
-];
 
 
 // Função para preencher a tabela com o array de objetos
-function preencherTabela(arg) {
+export function preencherTabela(arg) {
     const exercicios = document.getElementById("tabela");
 
     for (const args of arg) {
+        console.log('oi')
         const novaLinha = exercicios.insertRow();
 
         const nomeCelula = novaLinha.insertCell();
@@ -42,5 +21,7 @@ function preencherTabela(arg) {
     }
 }
 
-// Chame a função para preencher a tabela assim que a página for carregada
-preencherTabela(chest);
+
+
+
+
