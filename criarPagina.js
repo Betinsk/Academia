2def555c218fcf5422a3ad2e1deecec862e785e9
betinsk <gmdust@hotmail.com>
2024-01-09 23:18:50 -0300

    Página de treino

--- criarPagina.js
+++ criarPagina.js
@@ -1,22 +1,31 @@
+import { createElementWithClass } from "./script.js";
+
+
 export function criarPagina(array) {
+    
     // Criação dos elementos HTML
     const html = document.createElement('html');
-    console.log(array)
     const head = document.createElement('head');
     const body = document.createElement('body');
     const title = document.createElement('title');
     const h1 = document.createElement('h1');
     const paragrafo = document.createElement('p');
-    const lista = document.createElement('ul');
+    const list = document.createElement('ul');
     const back = document.createElement('a')
 
     // Adição de conteúdo aos elementos
-    title.textContent = 'Página Gerada pela Função';
-    h1.textContent = 'Bem-vindo à minha página gerada pelo DOM!';
-    paragrafo.textContent = 'Esta página foi criada dinamicamente usando JavaScript e o DOM.';
+    title.textContent = array.type;
+    h1.textContent = array.type;
     back.href = 'workouts.html'
     back.textContent = 'BACK';
 
+     // Adição do vídeo
+     const videoElement = document.createElement('iframe');
+     videoElement.src = array.video;
+     videoElement.width = '560';
+     videoElement.height = '315';
+     videoElement.frameborder = '0';
+     videoElement.allowfullscreen = 'true';
 
     //Array completo
    /* array.forEach((info) => {
@@ -26,22 +35,20 @@ export function criarPagina(array) {
     });*/
 
         //Array na posição
-        const listItem = document.createElement('li');
-        listItem.textContent = array.name;
-        console.log(array)
-        lista.appendChild(listItem);
+        list.appendChild(createElementWithClass('li', 'list', array.name ));
+        list.appendChild(createElementWithClass('p', 'description', array.description))
 
 
     // Adição de elementos ao DOM
     head.appendChild(title);
     body.appendChild(h1);
     body.appendChild(paragrafo);
-    body.appendChild(lista);
-    body.append(back)
+    body.appendChild(list);
+    body.appendChild(videoElement); // Adiciona o elemento de vídeo ao corpo
+    body.appendChild(back)
     html.appendChild(head);
     html.appendChild(body);
   
-
     // Limpa o conteúdo existente do corpo antes de adicionar o novo conteúdo
     document.body.innerHTML = '';
 
